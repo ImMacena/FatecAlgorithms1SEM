bb999af5925215be5997f985cdc6e73b8bf55405
ImMacena <guilhermewill92@gmail.com>
2024-05-15 02:04:34 -0300

    Fix List1B ex14 identation

--- List1B/Codes/ex14.c
+++ List1B/Codes/ex14.c
@@ -3,86 +3,86 @@
 #include <stdbool.h>
 #include <string.h>
 
-int main ()
+int main()
 {
-    // Um vendedor necessita de um algoritmo que calcula o pre鏾 total devido por um cliente.
-    // o algoritmo deve receber o c骴igo do produto e a quantidade comprada.
-    // Ap髎, calcular o pre鏾 total.
+  // Um vendedor necessita de um algoritmo que calcula o pre莽o total devido por um cliente.
+  // o algoritmo deve receber o c贸digo do produto e a quantidade comprada.
+  // Ap贸s, calcular o pre莽o total.
 
-    // C骴igo 1001 PU: 5.32
-    // C骴igo 1324 PU: 6.45
-    // C骴igo 6548 PU: 2.37
-    // C骴igo 0987 PU: 5.32
-    // C骴igo 7623 PU: 6.45
+  // C贸digo 1001 PU: 5.32
+  // C贸digo 1324 PU: 6.45
+  // C贸digo 6548 PU: 2.37
+  // C贸digo 0987 PU: 5.32
+  // C贸digo 7623 PU: 6.45
 
-    float totalAmount = 0;
-    int quantity, productQuantity = 0, opt = 1;
-    char code[6];
-    char codes[5][5] = {"1001", "1324", "6548", "0987", "7623"};
-    char prices[5][5] = {"5.32", "6.45", "2.37", "5.32", "6.45"};
+  float totalAmount = 0;
+  int quantity, productQuantity = 0, opt = 1;
+  char code[6];
+  char codes[5][5] = {"1001", "1324", "6548", "0987", "7623"};
+  char prices[5][5] = {"5.32", "6.45", "2.37", "5.32", "6.45"};
 
-    while (opt != 2)
+  while (opt != 2)
+  {
+    if (opt == 1)
     {
-        if (opt == 1)
+      bool hasCode = false;
+      int priceIndex;
+
+      system("cls");
+
+    codeInput:
+      printf("Enter the product code: ");
+      scanf(" %s", &code);
+
+      for (int i = 0; i < sizeof(codes) / sizeof(codes[0]); i++)
+      {
+        if (strcmp(codes[i], code) == 0)
         {
-            bool hasCode = false;
-            int priceIndex;
+          priceIndex = i;
 
-            system("cls");
+          hasCode = true;
 
-            codeInput:
-            printf("Enter the product code: ");
-            scanf(" %s", &code);
-
-            for (int i = 0; i < sizeof(codes) / sizeof(codes[0]); i++)
-            {
-                if (strcmp(codes[i], code) == 0)
-                {
-                    priceIndex = i;
-
-                    hasCode = true;
-
-                    break;
-                }
-            }
-
-            if (!hasCode)
-            {
-                system("cls");
-
-                printf("Error: this code is not available! Try again.\n");
-
-                goto codeInput;
-            }
-
-            system("cls");
-
-            quantityInput:
-            printf("Enter the quantity: ");
-            scanf("%d", &quantity);
-
-            if (quantity == 0 )
-            {
-                goto quantityInput;
-            }
-
-            totalAmount += (atof(prices[priceIndex]) * quantity);
-            productQuantity += quantity;
+          break;
         }
+      }
 
+      if (!hasCode)
+      {
         system("cls");
 
-        printf(" - Purchase sumary -\n");
+        printf("Error: this code is not available! Try again.\n");
 
-        printf("\nProducts quantity: %d", productQuantity);
-        printf("\nTotal amount: $%.2f \n", totalAmount);
+        goto codeInput;
+      }
 
-        printf("\n1 - Add product\n");
-        printf("2 - Exit the program\n");
+      system("cls");
 
-        printf("\nSelect an option: ");
-        scanf("%d", &opt);
+    quantityInput:
+      printf("Enter the quantity: ");
+      scanf("%d", &quantity);
+
+      if (quantity == 0)
+      {
+        goto quantityInput;
+      }
+
+      totalAmount += (atof(prices[priceIndex]) * quantity);
+      productQuantity += quantity;
     }
 
-    return 0;
+    system("cls");
+
+    printf(" - Purchase sumary -\n");
+
+    printf("\nProducts quantity: %d", productQuantity);
+    printf("\nTotal amount: $%.2f \n", totalAmount);
+
+    printf("\n1 - Add product\n");
+    printf("2 - Exit the program\n");
+
+    printf("\nSelect an option: ");
+    scanf("%d", &opt);
+  }
+
+  return 0;
 }
